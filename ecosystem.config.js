eab9ffa656fdf91a7eeee62b617fbb262a4f1154
Jeffrey Ryan Walker <jryanwalker93@gmail.com>
2020-08-23 17:24:44 -0500

    Updates connection string in ecosystem.config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-218-79-61.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-191-116-74.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/FEC-BestBuy-RW.pem',
       ref: 'origin/master',
       repo: 'https://github.com/ATX-50-Team-Best-Buy/rw_proxy.git',
